853ed10f598bd95b4c1367c1552428e0190d0229
Tudor <pagu.tudor@gmail.com>
2023-07-17 17:22:16 +0300

    Add guest account

--- components/SignInModal.tsx
+++ components/SignInModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Input, Modal, TextField } from '@mui/material'
 import Button from './ButtonTailwind'
 import { signInAsGuest, signInWithGoogle } from '@/app/fireauth'
@@ -11,6 +11,11 @@ type Props = {
 
 export default function EditProfileModal({ open, handleClose }: Props) {
 
+    const [guestName, setGuestName] = useState("Guest")
+    const onChangeGuestName = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setGuestName(e.target.value)
+    }
+
     const guestSignIn = () => {
         signInAsGuest("Guest")
     }
@@ -18,23 +23,28 @@ export default function EditProfileModal({ open, handleClose }: Props) {
         signInWithGoogle()
     }
 
+    const submitGuestForm: React.FormEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault()
+        signInAsGuest(guestName)
+
+    }
     return (
         <Modal className="flex justify-center items-center" open={open} onClose={handleClose} aria-labelledby="modal-modal-edit-profile" aria-describedby="modal-modal-description">
             <div className='bg-white rounded-md p-6'>
                 <h1 className='pb-4'>
                     Sign in
                 </h1>
-                <form>
-                    <div className='flex flex-col gap-3'>
-                        <div className="flex gap-4">
-                            <TextField label="username" variant="outlined" />
-                            <Button onClick={guestSignIn}>Sign in as guest</Button>
-                        </div>
-                        <Button onClick={googleSignIn}>
-                            Sign in with Google
-                        </Button>
-                    </div>
-                </form>
+
+                <div className='flex flex-col gap-3'>
+                    <form onSubmit={submitGuestForm} className="flex gap-4">
+                        <TextField onChange={onChangeGuestName} value={guestName} label="username" variant="outlined" />
+                        <Button onClick={() => { }}>Sign in as guest</Button>
+                    </form>
+                    <Button onClick={googleSignIn}>
+                        Sign in with Google
+                    </Button>
+                </div>
+
             </div>
         </Modal>
     )
