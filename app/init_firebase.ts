28818d7c16604158d78e22abe17d2a954edcc9af
Tudor <pagu.tudor@gmail.com>
2023-08-04 16:30:38 +0300

    Remove env variables

--- app/init_firebase.ts
+++ app/init_firebase.ts
@@ -2,16 +2,8 @@ import { initializeApp } from "firebase/app";
 import { collection, connectFirestoreEmulator, doc, getDocs, getFirestore, setDoc } from 'firebase/firestore';
 import { connectAuthEmulator, getAuth } from 'firebase/auth';
 import { getStorage } from "firebase/storage";
+import {firebaseConfig} from "../firebase_config"
 
-
-const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId
-}
 const app = initializeApp(firebaseConfig)
 
 const db = getFirestore(app)
